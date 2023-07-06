58fecd2cb6407d299597e5a10f0e5b6146033636
azam golestan <goli.goli499a.com>
2023-07-06 12:41:18 +0330

    Start section 11

---
+++ 11-advanced concepts of Nextjs/153-check the title and meta tags in the head/pages/next.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+function NextJS(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJS course</title>
+        <meta
+          name="description"
+          content="NextJS course"
+        />
+      </Head>
+      <h1>NextJS Course</h1>
+    </div>
+  );
+}
+
+export default NextJS;
