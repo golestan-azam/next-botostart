01dd8d8c54555f9718c145d3645830411287bfd9
azam golestan <goli.goli499a.com>
2023-08-15 17:16:30 +0330

    Completed section 22

---
+++ 22-user authentication (login and registration form) - real estate project/326-add session provider in next version 13/src/app/(auth)/signup/page.js
@@ -0,0 +1,7 @@
+import SignUpPage from "@/template/SignUpPage";
+
+function SignUp(props) {
+    return <SignUpPage />
+}
+
+export default SignUp;
---
+++ 22-user authentication (login and registration form) - real estate project/327-authentication in the header on the client side/src/app/(auth)/signin/page.js
@@ -0,0 +1,7 @@
+import SignInPage from "@/template/SignInPage";
+
+function SignIn(props) {
+    return <SignInPage />
+}
+
+export default SignIn;
